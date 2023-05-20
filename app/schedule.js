d33170f3f053a426cee7062d7695cd6669753682
Glitch (hydra-meetup-2) <none>
2023-05-20 14:23:35 +0000

    🚋🏎 Checkpoint ./app/schedule.js:9838890/2968 ./app/filter.js:9838890/4 ./package.json:9838890/1390 ./build/style.css:9838890/3188

--- app/schedule.js
+++ app/schedule.js
@@ -1,6 +1,106 @@
 const html = require("choo/html");
+const { css } = require("@emotion/css");
+
 const contents = require("./contents.js");
-// const contents = window.contents;
+
+const mainCss = css`
+
+section {
+  margin: 150px auto 150px auto;
+}
+
+
+.caption-holder {
+  max-width: 700px;
+  box-sizing:content-box;
+  background-color: white;
+  margin: 80px auto 200px auto;
+}
+
+.caption {
+  margin: 80px auto 200px auto;
+  width: 90%;
+/*   padding: 30px 10px 30px 10px; */
+/*   box-sizing:content-box; */
+  box-shadow: 0 0.5px 2px #000;
+  -webkit-box-shadow: 0 0.5px 2px #000;
+  padding: 30px 10px 10px 10px;
+}
+
+.thumbnail {
+  width: 100%;
+	align-items: baseline;
+  /*   min-width: 700px; */
+  display: flex;
+  justify-content: center;
+}
+
+.thumbnail > p {
+  font-family: "HK Grotesk", arial, sans-serif;
+  text-align: center;
+  font-style: italic;
+  background-color: #ccc;
+  display: table-cell;
+  vertical-align: middle;
+  position: relative;
+	width:100vw;
+  height:100px;
+  max-width: 800px;
+}
+
+.thumbnail > img,
+.rowimg > img {
+  width: 100%;
+  height: auto;
+  max-width: 800px;
+}
+
+.youtube-container {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 56.25%;
+  margin: 1.5rem 0 0 0;
+}
+
+.youtube-video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.collabs {
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: 50px;
+}
+
+.title {
+  font-style: italic;
+  left: -2px;
+  position: relative;
+}
+
+.venue {
+  margin-bottom: 50px;
+}
+
+.desc {
+  font-size: 12pt;
+  margin-bottom: 50px;
+/*   text-align: justify; */
+}
+
+.links {
+  font-size: 12pt;
+  a {
+    margin-right: 20px;
+  }
+}
+
+`;
 
 module.exports = () => {
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
@@ -78,7 +178,7 @@ module.exports = () => {
 
     dates.push(
       {type, title, dateYear, dom: html`
-        <section id="section-${i}">
+        <section class=${ mainCss } id="section-${i}">
           <div class="thumbnail">${imageElt}</div>
           <div class="caption-holder">
             <div class="caption">
